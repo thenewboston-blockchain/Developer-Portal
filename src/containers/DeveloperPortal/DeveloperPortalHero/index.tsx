2825713b5e8b3485d9afaaa7e8ce45a49e69e96d
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-09-30 00:31:24 +0530

    chore: minor fixes

--- src/containers/DeveloperPortal/DeveloperPortalHero/index.tsx
+++ src/containers/DeveloperPortal/DeveloperPortalHero/index.tsx
@@ -2,10 +2,16 @@ import React, {FC} from 'react';
 import {Icon, IconType} from '@thenewboston/ui';
 
 import {Button} from 'components';
+import {URLS} from 'constants/routes';
+import {socialMediaUrls} from 'utils/social-media';
 
 import './DeveloperPortalHero.scss';
 
 const DeveloperPortalHero: FC = () => {
+  const handleProposeProjectClick = () => window.open(URLS.github.proposeProjects, '_blank', 'noreferrer noopener');
+
+  const handleDiscordClick = () => window.open(socialMediaUrls.discord, '_blank', 'noreferrer noopener');
+
   return (
     <div className="DeveloperPortalHero">
       <h1 className="DeveloperPortalHero__title">
@@ -14,10 +20,15 @@ const DeveloperPortalHero: FC = () => {
       </h1>
       <p className="DeveloperPortalHero__description">Explore our guides and examples to integrate TNBC.</p>
       <div className="DeveloperPortalHero__buttons">
-        <Button color="quaternary" className="DeveloperPortalHero__button">
+        <Button color="quaternary" className="DeveloperPortalHero__button" onClick={handleProposeProjectClick}>
           Propose Project
         </Button>
-        <Button className="DeveloperPortalHero__button" color="quaternary" variant="outlined">
+        <Button
+          className="DeveloperPortalHero__button"
+          color="quaternary"
+          variant="outlined"
+          onClick={handleDiscordClick}
+        >
           <Icon icon={IconType.discord} />
           Discord
         </Button>

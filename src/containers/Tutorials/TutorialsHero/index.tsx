059879eb44f3389d72d629315397cb69ccfad14e
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2022-02-09 22:56:37 +0530

    fix: add redirection for projects

--- src/containers/Tutorials/TutorialsHero/index.tsx
+++ src/containers/Tutorials/TutorialsHero/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
 import {Button, VideoPlayer} from 'components';
+import {ROUTES} from 'constants/routes';
 import {Source} from 'types/tutorials';
 
 import * as S from './styles';
 
 const TutorialsHero = () => {
+  const history = useHistory();
+
   return (
     <S.Container>
       <S.Player>
@@ -17,7 +21,9 @@ const TutorialsHero = () => {
           Watch free coding tutorials created by thenewboston YouTube channel to help you develop apps in any language
           and earn TNBC.
         </S.Paragraph>
-        <Button color="quaternary">Learn How to Submit an App for TNBC {'-->'} </Button>
+        <Button color="quaternary" onClick={() => history.push(ROUTES.projects.home)}>
+          Learn How to Submit an App for TNBC {'-->'}{' '}
+        </Button>
       </S.Content>
     </S.Container>
   );
